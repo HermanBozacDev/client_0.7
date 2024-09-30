fde76b24c6a4f4eb23fd0ef942102da3c62b8833
HermanBozacDev <martinhermanbozac@gmail.com>
2024-09-30 17:52:53 -0300

    Update AdminPanel.jsx

--- src/Components/AdminPanel/AdminPanel.jsx
+++ src/Components/AdminPanel/AdminPanel.jsx
@@ -120,7 +120,7 @@ const AdminPanel = () => {
       case 'Users':
         return renderUsers();
       case 'Settings':
-        return <h2>Settings</h2>;
+        return <Register />;
       default:
         return <h2>Dashboard</h2>;
     }
