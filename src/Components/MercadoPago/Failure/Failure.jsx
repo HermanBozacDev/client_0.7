40008e2cfcd184aa9015147dc2c4f7272e901477
HermanBozacDev <martinhermanbozac@gmail.com>
2024-10-03 02:23:30 -0300

    Update Failure.jsx

--- src/Components/MercadoPago/Failure/Failure.jsx
+++ src/Components/MercadoPago/Failure/Failure.jsx
@@ -1,1 +1,15 @@
 
+import React from 'react'
+
+
+export const Failure = () => {
+  return (
+    <div className='contenedorFailure'>
+        <p>Failure</p>
+
+    </div>
+  )
+}
+
+
+export default Failure;
