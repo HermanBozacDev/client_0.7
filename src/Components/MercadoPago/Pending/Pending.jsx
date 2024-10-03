1d53a72670d06bac177b4ccca01bb24927784d3e
HermanBozacDev <martinhermanbozac@gmail.com>
2024-10-03 02:22:42 -0300

    Update Pending.jsx

--- src/Components/MercadoPago/Pending/Pending.jsx
+++ src/Components/MercadoPago/Pending/Pending.jsx
@@ -1,1 +1,12 @@
 
+import React from 'react'
+
+export const Pending = () => {
+  return (
+    <div className='contenedorPending'>
+        <p>Pending......</p>
+    </div>
+  )
+}
+
+export default Pending;
