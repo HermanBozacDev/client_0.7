8cbe75389fee5e625929107f2bd4276b5771116d
HermanBozacDev <martinhermanbozac@gmail.com>
2024-10-03 02:12:21 -0300

    Update Login.jsx

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -7,6 +7,7 @@ const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate(); // Inicializa useNavigate
+  console.log("ASD")
 
   const handleLogin = async (e) => {
     e.preventDefault();
