00076e54ce826d5e713c23701a0f1c848235bc75
HermanBozacDev <martinhermanbozac@gmail.com>
2024-10-03 02:41:26 -0300

    Update AdminLogin.jsx

--- src/Components/AdminPanel/AdminLogin/AdminLogin.jsx
+++ src/Components/AdminPanel/AdminLogin/AdminLogin.jsx
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom'; // Importa useNavigate para redirigir
-import '../AdminLogin/AdminLogin.css'; // Importamos los estilos
+import './AdminLogin.css'; // Importamos los estilos
 
 const AdminLogin = () => {
   const [username, setUsername] = useState('');
