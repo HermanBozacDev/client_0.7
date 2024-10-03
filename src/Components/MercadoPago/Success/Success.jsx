618c55b5a5d35f9e9dfc4ec57a948c7f7933b13b
HermanBozacDev <martinhermanbozac@gmail.com>
2024-10-03 02:23:05 -0300

    Update Success.jsx

--- src/Components/MercadoPago/Success/Success.jsx
+++ src/Components/MercadoPago/Success/Success.jsx
@@ -1,1 +1,14 @@
 
+import React from 'react'
+
+export const Success = () => {
+  return (
+    <div className='contenedorSuccess'>
+        <p>Success</p>
+
+    </div>
+  )
+}
+
+
+export default Success;
