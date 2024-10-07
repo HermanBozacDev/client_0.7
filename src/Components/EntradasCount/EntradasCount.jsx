db2a989eed9a7d6cc6918b7cd920b9307a591363
LeonardoRiera <159967149+LeonardoRiera@users.noreply.github.com>
2024-10-07 17:30:06 -0300

    Update EntradasCount.jsx

--- src/Components/EntradasCount/EntradasCount.jsx
+++ src/Components/EntradasCount/EntradasCount.jsx
@@ -10,7 +10,7 @@ const EntradasCount = ({ count, increment, decrement }) => {
   return (
     <div className="Controls">
 
-        <button className="ButtonContador" onClick={decrement}>-</button>
+        <button className="ButtonContadorM" onClick={decrement}>-</button>
         <h4 className="Number">{count}</h4>
         <button className="ButtonContador" onClick={increment}>+</button>
     </div>
