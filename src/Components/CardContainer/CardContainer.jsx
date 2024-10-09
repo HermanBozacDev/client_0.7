b4d9f8c066f39382f4fd92903ec74be93297a4a7
HermanBozacDev <martinhermanbozac@gmail.com>
2024-10-09 19:07:02 -0300

    Update CardContainer.jsx

--- src/Components/CardContainer/CardContainer.jsx
+++ src/Components/CardContainer/CardContainer.jsx
@@ -1,39 +1,54 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios'; // Importa Axios directamente
-import ItemCards from '../ItemCards/ItemCards.jsx';
+import Card from '../Card/Card'; // Asegúrate de importar el componente Card
 import '../CardContainer/CardContainer.css';
 import Header from '../Header/Header.jsx';
 
 const CardContainer = () => {
-  const [productos, setProductos] = useState([]);
+  const [eventos, setEventos] = useState([]); // Cambia 'productos' a 'eventos'
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  // Función asíncrona que obtiene los productos de la API
-  const fetchProductos = async () => {
+  // Función asíncrona que obtiene los eventos de la API
+  const fetchEventos = async () => {
     try {
-      const response = await axios.get('https://www.imperioticket.com/api/eventos'); // Cambia 'http://tu-api-url' por tu URL real
-      setProductos(response.data);
+      const response = await axios.get('https://www.imperioticket.com/api/eventos'); // Cambia por tu URL real
+      setEventos(response.data);
     } catch (error) {
-      setError("Error fetching productos: " + error.message);
+      setError("Error fetching eventos: " + error.message);
     } finally {
       setLoading(false);
     }
   };
 
-  // useEffect para cargar los productos cuando el componente se monte
+  // useEffect para cargar los eventos cuando el componente se monte
   useEffect(() => {
-    fetchProductos(); // Llamamos a la función async dentro de useEffect
+    fetchEventos(); // Llamamos a la función async dentro de useEffect
   }, []);
 
-  if (loading) return <div>Cargando productos...</div>;
+  if (loading) return <div>Cargando eventos...</div>;
   if (error) return <div>{error}</div>;
 
   return (
     <div className='CardContainer'>
       <Header />
       <div id='ShowsScroll'></div>
-      <ItemCards productos={productos} />
+      {eventos.map((evento) => (
+        <Card
+          key={evento._id}
+          title={evento.title}
+          price={evento.price}
+          image={evento.image}
+          image2={evento.image2}
+          imageDetail={evento.imageDetail}
+          dia={evento.dia}
+          fecha={evento.fecha}
+          hora={evento.hora}
+          lugar={evento.lugar}
+          description={evento.description}
+          clasificacion={evento.clasificacion}
+        />
+      ))}
     </div>
   );
 };
