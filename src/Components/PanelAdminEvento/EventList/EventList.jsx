fa1defadedd63bcb125db15547722d7cb06c8d1b
HermanBozacDev <martinhermanbozac@gmail.com>
2024-10-09 23:17:55 -0300

    Create EventList.jsx

---
+++ src/Components/PanelAdminEvento/EventList/EventList.jsx
@@ -0,0 +1,13 @@
+const EventList = ({ eventos, onDelete }) => (
+  <div>
+    <h2>Eventos Existentes</h2>
+    <ul>
+      {eventos.map((evento) => (
+        <li key={evento._id}>
+          <h3>{evento.title}</h3>
+          <button onClick={() => onDelete(evento._id)}>Eliminar Evento</button>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
