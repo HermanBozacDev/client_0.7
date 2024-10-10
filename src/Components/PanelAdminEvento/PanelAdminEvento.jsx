7371b7a29d03f4f5778dcdb3ca4beb1ba49b3c29
HermanBozacDev <martinhermanbozac@gmail.com>
2024-10-09 22:10:49 -0300

    Update PanelAdminEvento.jsx

--- src/Components/PanelAdminEvento/PanelAdminEvento.jsx
+++ src/Components/PanelAdminEvento/PanelAdminEvento.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import '../PanelAdminEvento/PanelAdminEvento.css';
-import UseEventos from '../UseEventos/UseEventos';
+import UseEventos from './UseEventos/UseEventos';
 
 
 const PanelAdminEvento = () => {
