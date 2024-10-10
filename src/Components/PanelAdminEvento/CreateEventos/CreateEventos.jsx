e4ac130c76db678e2fb0d166664e307ca9d3b5f5
HermanBozacDev <martinhermanbozac@gmail.com>
2024-10-09 22:26:30 -0300

    Update CreateEventos.jsx

--- src/Components/PanelAdminEvento/CreateEventos/CreateEventos.jsx
+++ src/Components/PanelAdminEvento/CreateEventos/CreateEventos.jsx
@@ -1,1 +1,55 @@
 
+import axios from 'axios';
+
+const useCrearEvento = (
+  nuevoEvento,
+  setNuevoEvento,
+  setSelectedImage1,
+  setSelectedImage2,
+  setImagesUploaded,
+  setFeedbackMessage,
+  obtenerEventos
+) => {
+  const crearEvento = async (e) => {
+    e.preventDefault();
+    const { title, clasificacion, description, dia, fecha, hora, image, image2, imageDetail, lugar, price, quantity } = nuevoEvento;
+    if (!title || !clasificacion || !description || !dia || !fecha || !hora || !image || !image2 || !imageDetail || !lugar || !price || !quantity) {
+      setFeedbackMessage('Por favor, completa todos los campos requeridos.');
+      return;
+    }
+
+    try {
+      await axios.post('https://www.imperioticket.com/api/eventos', nuevoEvento, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      setNuevoEvento({
+        clasificacion: '',
+        description: '',
+        dia: '',
+        fecha: '',
+        hora: '',
+        image: '',
+        image2: '',
+        imageDetail: '',
+        lugar: '',
+        price: '',
+        quantity: '',
+        title: ''
+      });
+      setSelectedImage1(null);
+      setSelectedImage2(null);
+      setImagesUploaded(false);
+      setFeedbackMessage('Evento creado con éxito.');
+      obtenerEventos();
+    } catch (error) {
+      console.error('Error al crear evento:', error);
+      setFeedbackMessage('Error al crear evento. Por favor, intenta de nuevo.');
+    }
+  };
+
+  return { crearEvento };
+};
+
+export default useCrearEvento;
