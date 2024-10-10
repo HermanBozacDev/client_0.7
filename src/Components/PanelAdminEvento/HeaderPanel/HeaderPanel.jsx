42794c2abe41f5dabe49c864cc561fd27b78a812
HermanBozacDev <martinhermanbozac@gmail.com>
2024-10-09 23:15:54 -0300

    Create HeaderPanel.jsx

---
+++ src/Components/PanelAdminEvento/HeaderPanel/HeaderPanel.jsx
@@ -0,0 +1,7 @@
+const HeaderPanel = ({ onLogout }) => (
+  <div>
+    <h1>Panel de Administración de Eventos</h1>
+    <button onClick={onLogout}>Cerrar Sesión</button>
+    <h3>Esta funcionando la base de datos, puedes crear eventos, eliminarlos, modificarlos, y buscar por día, descripción o nombre. Estamos trabajando para usted.</h3>
+  </div>
+);
